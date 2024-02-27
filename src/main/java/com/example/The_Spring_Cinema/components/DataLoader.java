16a499df55d62bcbed405e294cd5f5119de7e4b6
Yesica Nithiyanantha <y.nithi123@gmail.com>
2024-02-27 16:46:59 +0000

    Created a reply class.

--- src/main/java/com/example/The_Spring_Cinema/components/DataLoader.java
+++ src/main/java/com/example/The_Spring_Cinema/components/DataLoader.java
@@ -1,4 +1,22 @@
 package com.example.The_Spring_Cinema.components;
 
-public class DataLoader {
+import com.example.The_Spring_Cinema.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    MovieService movieService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+        movieService.addNewMovie();
+        movieService.addNewMovie();
+
+    }
 }
--- src/main/java/com/example/The_Spring_Cinema/controllers/MovieController.java
+++ src/main/java/com/example/The_Spring_Cinema/controllers/MovieController.java
@@ -1,12 +1,17 @@
 package com.example.The_Spring_Cinema.controllers;
 
+import com.example.The_Spring_Cinema.models.Movie;
+import com.example.The_Spring_Cinema.models.Reply;
 import com.example.The_Spring_Cinema.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping(value = "/movies")
@@ -16,7 +21,8 @@ public class MovieController {
     MovieService movieService;
 
     @PostMapping
-//    public ResponseEntity<Reply> newMovie(){
-//        Reply reply = movieService.addNewMovie();
-//        return new ResponseEntity<>(reply, HttpStatus.CREATED);
+    public ResponseEntity<Reply> newMovie(@RequestBody ArrayList<Movie> movies) {
+        Reply reply = movieService.addNewMovie();
+        return new ResponseEntity<>(reply, HttpStatus.CREATED);
+    }
 }
